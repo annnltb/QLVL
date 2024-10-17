87219d63ffd2639b71d81a905a5561185f2d5db5
annnltb <annn.ltb@gmail.com>
2024-10-17 10:05:19 +0700

    feature: something

---
+++ QLVL/View/Employee/Plan/PlanForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLVL.View.Employee
+{
+    public partial class PlanForm : Form
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Quanlyvatlieu_doantichhop;Integrated Security=True";
+
+        public PlanForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Plan_Load(object sender, EventArgs e)
+        {
+            LoadInventoryPlans();
+        }
+
+        private void LoadInventoryPlans()
+        {
+            string query = "SELECT * FROM InventoryPlans";
+            DataTable plansTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                connection.Open();
+                adapter.Fill(plansTable);
+                dataGridViewPlans.DataSource = plansTable;
+
+                // Đặt tên cột cho DataGridView
+                string[] columnNames = { "Mã Kế Hoạch", "Mã Vật Liệu", "Mã người dùng", "Số lượng", "Ngày tạo kế hoạch", "Ngày Dự Kiến Hoàn Thành" };
+                for (int i = 0; i < columnNames.Length && i < dataGridViewPlans.Columns.Count; i++)
+                {
+                    dataGridViewPlans.Columns[i].HeaderText = columnNames[i];
+                }
+            }
+        }
+    }
+    }
--- QLVL/View/Employee/StockTransaction.cs
+++ QLVL/View/Employee/StockTransaction.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using QLVL.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,101 @@ namespace QLVL.View.Employee
 {
     public partial class StockTransaction : Form
     {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Quanlyvatlieu_doantichhop;Integrated Security=True";
+        
         public StockTransaction()
         {
             InitializeComponent();
+            
+        }
+
+        public void LoadTransaction()
+        {
+
+            string query = "SELECT * FROM Transactions";
+            DataTable transactionsTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(transactionsTable);
+
+
+
+                    // Gán DataTable cho DataGridView
+                    dataGridViewTransaction.DataSource = transactionsTable;
+
+                    // Đặt tên cột bằng mảng
+                    string[] columnNames = { "Mã giao dịch", "Mã vật liệu", "Mã nhà cung cấp", "Mã người dùng", "Số lượng", "Loại giao dịch", "Thời gian", "Mô tả" }; // Thay đổi tên cột theo nhu cầu của bạn
+                    for (int i = 0; i < columnNames.Length && i < dataGridViewTransaction.Columns.Count; i++)
+                    {
+                        dataGridViewTransaction.Columns[i].HeaderText = columnNames[i];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+                }
+            }
+        }
+        private void StockTransaction_Load(object sender, EventArgs e)
+        {
+            LoadTransaction();
+            comboBox_type.Items.Add("Nhập");
+            comboBox_type.Items.Add("Xuất");
+
+        }
+
+        private void btn_sx_Click(object sender, EventArgs e)
+        {
+            if (comboBox_type.SelectedItem != null)
+    {
+        string transactionType = comboBox_type.SelectedItem.ToString();
+        DataTable transactionTable = new DataTable();
+
+        // Câu truy vấn để lấy giao dịch theo loại
+        string query = "SELECT * FROM Transactions WHERE TransactionType = @TransactionType";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TransactionType", transactionType);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            try
+            {
+                connection.Open();
+                adapter.Fill(transactionTable);
+
+                // Gán DataTable cho DataGridView
+                dataGridViewTransaction.DataSource = transactionTable;
+
+                // Đặt tên cột bằng mảng
+                string[] columnNames = { "Mã giao dịch", "Mã vật liệu", "Mã nhà cung cấp", "Mã người dùng", "Số lượng", "Loại giao dịch", "Thời gian", "Mô tả" }; // Thay đổi tên cột theo nhu cầu của bạn
+                for (int i = 0; i < columnNames.Length && i < dataGridViewTransaction.Columns.Count; i++)
+                {
+                    dataGridViewTransaction.Columns[i].HeaderText = columnNames[i];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+            }
+        }
+    }
+    else
+    {
+        MessageBox.Show("Vui lòng chọn loại giao dịch trước!");
+    }
+
+        }
+
+        private void dataGridViewTransaction_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
